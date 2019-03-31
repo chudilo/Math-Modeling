43313a0f636c72e9528eae477925e38d97319178
chudilo <chudik97@yandex.ru>
2019-03-31 22:43:16 +0300

    Fixed Simpson

--- lab2/main.py
+++ lab2/main.py
@@ -1,24 +1,21 @@
 def my_func(x):
 	return x
 
-# calculating an integral by Simpson's method	
+# calculating an integral by Simpson's method
 def simpsonIntegr(a, b, func, stepcnt=41):
 	if(stepcnt and stepcnt%2 == 1):
 		step = (b-a)/(stepcnt-1)
-		ret = 0
+		ret = func(a) + func(b)
 
-		for i in range(stepcnt):
-			if i == 0 or i == stepcnt:
-				ret += func(a) 
-			elif i%2 == 1:
+		for i in range(1, stepcnt-1):
+			a += step
+			if i%2 == 1:
 				ret += 4*func(a)
 			else:
 				ret += 2*func(a)
 
-			a += step
-
 		return ret*step/3
-		
-		
+
+
 if __name__ == "__main__":
 	print(simpsonIntegr(0, 5, my_func))
